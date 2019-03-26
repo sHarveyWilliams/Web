29a5666fe583a18d7fa4e7199cb052bc23d3e2c5
Yaroslav Maximov <sHarveyWilliams@gmail.com>
2019-03-26 17:33:02 +0300

    Update script.js

--- Work 6/script.js
+++ Work 6/script.js
@@ -1,75 +1,83 @@
-var game = {
-		process: 1,
-		init: function(){
-		alert("Игра крестики нолики")
-		var arr1 = ["-", "-", "-"];
-		var arr2 = ["-", "-", "-"];
-		var arr3 = ["-", "-", "-"];
-		var BasicArr = new Array(arr1, arr2, arr3);
-		while(this.process>0){
-				alert("Ход крестиков")
-				i=game.getRowAndColumn();
-				j=game.getRowAndColumn();
-				if (BasicArr[i][j]=="-")
-					{
-						
-					}
-				else{
-					while((BasicArr[i][j]=="x")||(BasicArr[i][j]=="o")){
-						alert("Ячейка занята! Попробуйте другой ход");
-						i=game.getRowAndColumn();
-						j=game.getRowAndColumn();
-					}
-				}
-				game.getMoveX(BasicArr, i, j);
-				console.clear()
-				console.log(BasicArr.join('\n'));
-					if (((BasicArr[0][0]=="x"&BasicArr[0][1]=="x"&BasicArr[0][2]=="x")||(BasicArr[0][0]=="x"&BasicArr[1][0]=="x"&BasicArr[2][0]=="x")||((BasicArr[1][0]=="x"&BasicArr[1][1]=="x"&BasicArr[1][2]=="x")||(BasicArr[2][0]=="x"&BasicArr[2][1]=="x"&BasicArr[2][2])=="x")||(BasicArr[0][0]=="x"&BasicArr[1][1]=="x"&BasicArr[2][2]=="x")||(BasicArr[0][2]=="x"&BasicArr[1][1]=="x"&BasicArr[2][0])=="x")){
-						alert("Победа крестиков!"); 
-						this.process=0;
-						break;}
-							else{
-						
-						}
-				alert("Ход ноликов")
-				i=game.getRowAndColumn();
-				j=game.getRowAndColumn();
-				if (BasicArr[i][j]=="-")
-					{
-						
-					}
-				else{
-					while((BasicArr[i][j]=="x")||(BasicArr[i][j]=="o")){
-						alert("Ячейка занята! Попробуйте другой ход");
-						i=game.getRowAndColumn();
-						j=game.getRowAndColumn();
-					}
-				}
-				game.getMoveO(BasicArr, i, j);
-				console.clear()
-				console.log(BasicArr.join('\n'));
-					if (((BasicArr[0][0]=="o"&BasicArr[0][1]=="o"&BasicArr[0][2]=="o")||(BasicArr[0][0]=="o"&BasicArr[1][0]=="o"&BasicArr[2][0]=="o")||((BasicArr[1][0]=="o"&BasicArr[1][1]=="o"&BasicArr[1][2]=="o")||(BasicArr[2][0]=="o"&BasicArr[2][1]=="o"&BasicArr[2][2])=="o")||(BasicArr[0][0]=="o"&BasicArr[1][1]=="o"&BasicArr[2][2]=="o")||(BasicArr[0][2]=="o"&BasicArr[1][1]=="o"&BasicArr[2][0])=="o")){
-						alert("Победа ноликов!");
-						this.process=0;
-						break;}
-							else{
-						}
-			}
-		}
-	
-	};
-	
+/*
 
+26.03.2019
+*/
+
+
+var game = {
+		process: null,
+		playerX: 'Крестики',
+		playerO: 'Нолики',
+		
+		init: function(){
+		var process, arr1, arr2, arr3, allMoves, BasicArr;
+		arr1 = ["-", "-", "-"];
+		arr2 = ["-", "-", "-"];
+		arr3 = ["-", "-", "-"];
+		allMoves = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+		BasicArr = new Array(arr1, arr2, arr3);
+		
+		alert("Игра крестики нолики");
+		
+		for(var k=0; k<=9 ; k++){
+			if(allMoves[k]=='x'){
+				alert("Ход :" + this.playerX);
+				}
+			else{
+				alert("Ход :" + this.playerO);
+			}
+				i=this.getRowAndColumn();
+				j=this.getRowAndColumn();
+				
+				
+				
+				if (BasicArr[i][j]=="-")
+					{
+						BasicArr[i][j]=allMoves[k];
+					}
+				else{
+						while((BasicArr[i][j]=="x")||(BasicArr[i][j]=="o")){
+							alert("Ячейка занята! Попробуйте другой ход");
+							i=this.getRowAndColumn();
+							j=this.getRowAndColumn();
+						}
+						BasicArr[i][j]=allMoves[k];
+					}
+				
+				console.clear()
+				console.log(BasicArr.join('\n'));
+				
+				process=this.checkGame(BasicArr, allMoves[k])
+				
+				if(process=='win'){
+					break;
+				}
+		  
+		}
+	},
 	
-	game.getMoveX = function(BasicArr, i, j){
-			return BasicArr[i][j] = "x";
-	};
+	checkGame: function(BasicArr, value){
+			if (((BasicArr[0][0]==value&BasicArr[0][1]==value&BasicArr[0][2]==value)||
+			(BasicArr[1][0]==value&BasicArr[1][1]==value&BasicArr[1][2]==value)||
+			(BasicArr[2][0]==value&BasicArr[2][1]==value&BasicArr[2][2]==value)||
+			
+			(BasicArr[0][0]==value&BasicArr[1][0]==value&BasicArr[2][0]==value)||
+			(BasicArr[0][1]==value&BasicArr[1][1]==value&BasicArr[2][1]==value)||
+			(BasicArr[0][2]==value&BasicArr[1][2]==value&BasicArr[2][2]==value)||
+			
+			(BasicArr[0][0]==value&BasicArr[1][1]==value&BasicArr[2][2])==value)||
+			(BasicArr[0][2]==value&BasicArr[1][1]==value&BasicArr[2][0]==value))
+				{
+					alert("Победили: " + value);
+					return 'win';
+								}
+			else{
+								
+					return 'fall';
+								}
+	},
 	
-	game.getMoveO = function(BasicArr, i, j){
-			return BasicArr[i][j] = "o";
-	};
-	
-	game.getRowAndColumn = function(){
+	getRowAndColumn: function(){
 			value=+prompt("Введите значение");
 			if (value<=2){
 				return value;
@@ -80,7 +88,6 @@ var game = {
 				}
 				return value;
 			}
-	};
-	
+	}
+	}
 game.init()
-
